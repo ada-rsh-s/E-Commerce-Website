7b909f3bc9fe774357aada635fc12af2864be940
Adarsh-S1 <adarshvillasuni@gmail.com>
2022-07-07 20:20:49 +0530

    added products

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ const state={
 }
 module.exports.connect=function(done){
 
-    const url='mongodb://localhost:27017'
+    const url =
+      "mongodb+srv://adarsh:adarsh@mobihub.kkvqk.mongodb.net/?retryWrites=true&w=majority";
     const dbname='shopping'
 
     mongoClient.connect(url,(err,data)=>{
